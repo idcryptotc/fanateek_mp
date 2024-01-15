a22baba0c45cc403f1899f95f8e26d7f286a26b6
fanateek <idcryptotc@yandex.ru>
2024-01-15 05:43:25 +0300

    Добавлена первичная структура класса Player

--- fanateek_mp/player.cpp
+++ fanateek_mp/player.cpp
@@ -1,1 +1,57 @@
 #include "player.h"
+
+void Player::play()
+{
+}
+
+void Player::pause()
+{
+}
+
+void Player::stop()
+{
+}
+
+void Player::next()
+{
+}
+
+void Player::prev()
+{
+}
+
+void Player::volume_up()
+{
+}
+
+void Player::volume_down()
+{
+}
+
+void Player::set_position()
+{
+}
+
+void Player::set_position_step_left()
+{
+}
+
+void Player::set_position_step_right()
+{
+}
+
+void Player::set_repeat_state()
+{
+}
+
+void Player::set_shuffle_state()
+{
+}
+
+void Player::close_track()
+{
+}
+
+void Player::open_track()
+{
+}
--- fanateek_mp/player.h
+++ fanateek_mp/player.h
@@ -1,1 +1,23 @@
 #pragma once
+
+class Player
+{
+public:
+    static void play();
+    static void pause();
+    static void stop();
+    static void next();
+    static void prev();
+    static void volume_up();
+    static void volume_down();
+    static void set_position();
+    static void set_position_step_left();
+    static void set_position_step_right();
+    static void set_repeat_state();
+    static void set_shuffle_state();
+private:
+    bool is_repeat;
+    bool is_shuffle;
+    static void close_track();
+    static void open_track();
+};
